c36d3a96a521a634644e910ff00f58ca7fd30f40
malikraheelrugh <raheelrugh677@gmail.com>
2025-07-24 20:11:16 +0500

    updated more

--- index.js
+++ index.js
@@ -21,7 +21,13 @@ form =
     isRequired:false
 }
 ]
-const formContainer = document.getElementById("form");
+const h1 =  document.createElement('h1')
+h1.innerHTML='My first form in life with the help of Javascript.<br><i>Prepared by Raheel</i>'
+h1.style.color='white'
+h1.style.borderBottom='4px dotted green'
+//h1.style.fontFamily='sansSerif'
+document.body.append(h1)
+const firstForm = document.getElementById("form");
 const formElement = document.createElement("form");
 form.forEach(field => {
     const label = document.createElement("label");
@@ -33,7 +39,7 @@ form.forEach(field => {
     label.style.fontFamily='sansSerif'
     const input = document.createElement("input");
     input.type = field.type;
-    input.style.padding = '8px 50px 8px 5px';
+    input.style.padding = '8px 80px 8px 5px';
     input.style.color='white'
     input.style.borderRadius='7px'
     input.style.border='none'
@@ -45,7 +51,7 @@ form.forEach(field => {
     formElement.appendChild(label);
     formElement.appendChild(document.createElement("br"));
 });
-formContainer.appendChild(formElement);
+firstForm.appendChild(formElement);
 
 const btn = document.getElementById("btn")
 btn.style.backgroundColor='green'
@@ -61,7 +67,7 @@ formElement.style.display='flex'
 formElement.style.flexDirection='column'
 formElement.style.justifyContent='center'
 formElement.style.alignItems='center'
-formElement.style.padding='1.5rem 0'
+formElement.style.padding='1.2rem  0 1.7rem 0rem'
 formElement.style.borderRadius='6px'
 document.body.style.margin='0'
 document.body.style.padding='0'
@@ -72,7 +78,7 @@ document.body.style.position='relative'
 document.body.style.marginLeft='20%'
 document.body.style.marginRight='20%'
 document.body.style.marginTop='15%'
-document.body.style.background='black'
+document.body.style.background='rgba(12, 0, 0, 0.87)'
 btn.addEventListener('click' , function(){
     console.log('clicked me')
 })
